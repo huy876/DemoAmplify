ca9aa43df3a3f06fdd32804a8c34cba7e36a3c42
VU QUANG HUY <huyvq@OMINEXT.LOCAL>
2022-07-27 12:38:54 +0700

    initial_after_first_push

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "chillamplify": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
